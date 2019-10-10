af629bd207d37d63e3bca4a3ba82b64887808e12
ksyeon <ksyeon2019@gmail.com>
2019-10-10 17:16:42 +0900

    p.264 @Input 데코레이터와 setter를 이용한 입력 프로퍼티 조작

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -22,13 +22,41 @@ import { User } from '../models/user.model';
         </tr>
       </tbody>
     </table>
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <p>Admin: {{ cntAdmin }}</p>
+        <p>Developer: {{ cntDeveloper }}</p>
+        <p>Designer: {{ cntDesigner }}</p>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class UserListComponent implements OnInit {
   // 바인딩
+  //@Input() users: User[];
+
+  // _users는 내부에서만 사용할 private property
+  private _users: User[];
+
+  // 역할별 사용자 카운터
+  cntAdmin: number;
+  cntDeveloper: number;
+  cntDesigner: number;
+
   @Input()
-  users: User[];
+  set users (users: User[]) {
+    if (!users) { return; }
+
+    this.cntAdmin = users.filter(({role}) => role === "Administrator").length;
+    this.cntDeveloper = users.filter(({role}) => role === "Developer").length;
+    this.cntDesigner = users.filter(({role}) => role === "Designer").length;
+    this._users = users;
+  }
+
+  get users(): User[] {
+    return this._users;
+  }
 
   constructor() { }
 
