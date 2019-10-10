14c4e7da813f6235596f4ad7a1c7ef25352dcf61
ksyeon <ksyeon2019@gmail.com>
2019-10-10 16:59:24 +0900

    p.258 부모 컴포넌트에서 자식 컴포넌트로 상태 전달 - @Input 데코레이터

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ title }}</h1>
-    <app-user-list></app-user-list>
+    <div class="container">
+      <div class="row">
+        <div class="form-inline">
+          <div class="form-group" style="margin: 30px 0">
+            <label for="name">Name:</label>
+            <input #name type="text" id="name" 
+              class="form-control" placeholder="이름을 입력하세요">
+            <label for="role">Role:</label>
+            <select #role id="role" class="form-control">
+              <option>Administrator</option>
+              <option>Developer</option>
+              <option>Designer</option>
+            </select>
+            <button type="button"
+              class="btn btn-default"
+              (click)="addUser(name.value, role.value)">Add user
+            </button>
+          </div>
+          <app-user-list [users]="users"></app-user-list>
+        </div>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'component-interaction';
+  // 자식 컴포넌트와 공유할 상태 정보
+  users: User[];
+
+  constructor() {
+    this.users = [
+      new User(1, "Lee", "Administrator"),
+      new User(2, "Baek", "Developer"),
+      new User(3, "Park", "Designer")
+    ];
+  }
+
+  // 사용자 추가
+  addUser (name: string, role: string) {
+    if (name && role) {
+      this.users = [...this.users, new User(this.getNextId(), name, role)];
+    }
+  }
+
+  // 새로운 사용자의 id를 취득
+  getNextId(): number {
+    return this.users.length ? Math.max(...this.users.map(({ id }) => id)) + 1 : 1;
+  }
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user-list',
   template: `
-    <p>
-      user-list works!
-    </p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let user of users; let i=index">
+          <td>{{ i }}</td>
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.role }}</td>
+        </tr>
+      </tbody>
+    </table>
   `,
   styles: []
 })
 export class UserListComponent implements OnInit {
+  // 바인딩
+  @Input()
+  users: User[];
 
   constructor() { }
 
